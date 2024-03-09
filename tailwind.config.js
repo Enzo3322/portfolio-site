a9c8166edada605a9ccf69662562455d8c897f2d
Enzo3322 <enzo.spag14@gmail.com>
2024-03-09 19:23:18 -0300

    add tw

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: "#13508d",
+        secondary: "#0a66c2",
+      },
+    },
+  },
+  plugins: [],
+};
